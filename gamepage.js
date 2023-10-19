7e8e53e758fbef2be1ff24c2db148fd66620f609
federivadeneyra <143852422+federivadeneyra@users.noreply.github.com>
2023-10-18 20:34:04 -0600

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,20 @@
+// valor aleatorio generado
+var y = Math.floor(Math.random() * 10 + 1);
+var x = document.getElementById("guessField").value;
+// contar el número de intentos
+ var guess= 0
+  if (x == y) {
+
+
+
+    
+  }
+else IdleDeadline(x = y ){ 
+    guess ++; enter alert ("ups lo siento intenta un numero mas peqeueño" )
+}
+else IdleDeadline(x = y ){ 
+    guess ++; enter alert ("ups lo siento intenta un numero mas grande" )
+}
+function playAgain (){
+    y = math.floor (Math.random)
+}
